23924e308d1922e2af6d654cbf5b0afdcf1bd7f9
Blazze <blxzze-eg3@yahoo.com>
2024-11-02 19:21:49 +0100

    Test Substraction done

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -87,7 +87,28 @@ class CalculatorTest {
 
         assertEquals(expected, actual);
     }
+// TODO
 
+    @Test
+    @DisplayName("should display positiv outcome ")
+    void testSubstraction(){
+
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(1);
+        calc.pressDigitKey(3);
+
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(0);
+        calc.pressDigitKey(9);
+        calc.pressEqualsKey();
+
+        String expected = "4";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+
+    }
 
     //TODO hier weitere Tests erstellen
 }
